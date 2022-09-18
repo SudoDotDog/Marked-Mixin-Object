d67dfb2011445c4fb526e2d5fc4c612e2815976d
wmxpy <wm@mengw.io>
2022-09-18 01:48:47 -0500

    add object methods

--- src/methods.ts
+++ src/methods.ts
@@ -5,4 +5,14 @@
  */
 
 export const MarkedObjectMixinMethods: Record<string, any> = {
+
+    keys: (object: any): string[] => {
+        return Object.keys(object);
+    },
+    values: (object: any): any[] => {
+        return Object.values(object);
+    },
+    entries: (object: any): Array<[string, any]> => {
+        return Object.entries(object);
+    },
 };
